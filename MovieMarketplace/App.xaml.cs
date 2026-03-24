04d66436558cb2f68e71645b426b174dff3df9cc
georgianamariacalugar <georgiana.maria.calugar@stud.ubbcluj.ro>
2026-03-24 21:16:10 +0200

    updated

---
+++ MovieMarketplace/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace MovieMarketplace
+{
+    public partial class App : Application
+    {
+        // Ambele sunt acum 'settable' pentru a elimina eroarea CS0200
+        public static Window? m_window { get; set; }
+        public static Window? MainWindow { get; set; }
+
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            var window = new MainWindow();
+
+            m_window = window;
+            MainWindow = window;
+
+            window.Activate();
+        }
+    }
+}
